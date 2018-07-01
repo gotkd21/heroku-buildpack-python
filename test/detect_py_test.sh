f31934de914053049518dc4176a0a78d590b6027
dmfbrew <gotkd21@gmail.com>
2018-06-30 22:06:12 -0500

    fixed assertion

--- test/detect_py_test.sh
+++ test/detect_py_test.sh
@@ -3,8 +3,8 @@
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
 testDeterminePython27() {
-    touch ${HOME}/test-runtime.txt
-    echo "python-2.7.14" >| ${HOME}/test-runtime.txt
+    touch ${BUILD_DIR}/test-runtime.txt
+    echo "python-2.7.14" >| ${BUILD_DIR}/test-runtime.txt
 
     compile
 
