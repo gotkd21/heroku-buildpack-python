168b52596ac884f6daf2c8151aaf71ddaef24aa2
dmfbrew <gotkd21@gmail.com>
2018-06-30 20:52:50 -0500

    copied from Casey's test

--- test/detect_py_test.sh
+++ test/detect_py_test.sh
@@ -1,4 +1,18 @@
-# detect_py_test.sh
+#!/bin/bash
+
+. ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
+
+testDeterminePython27() {
+    touch ${BUILD_DIR}/test-runtime.txt
+    echo "python-2.7.14" >| ${BUILD_DIR}/test-runtime.txt
+
+    compile
+
+    assertContains "Unfortunately," "`cat ${STD_OUT}`"
+    assertContains "heroku stack:set heroku-16" "`cat ${STD_OUT}`"
+    assertEquals 1 ${rtrn}
+}
+
 source ..\utils.sh
 echo "This is a test" > $STD_OUT
 
