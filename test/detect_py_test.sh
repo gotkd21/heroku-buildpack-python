c84e778c419d43559e91284e3d2945d6375d710a
dmfbrew <gotkd21@gmail.com>
2018-06-30 21:37:10 -0500

    debugging env

--- test/detect_py_test.sh
+++ test/detect_py_test.sh
@@ -4,14 +4,11 @@ export
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
 testDeterminePython27() {
-    touch ${BUILD_DIR}/test-runtime.txt
-    echo "python-2.7.14" >| ${BUILD_DIR}/test-runtime.txt
+    touch ${HOME}/test-runtime.txt
+    echo "python-2.7.14" >| ${HOME}/test-runtime.txt
 
     compile
 
     assertContains "Unfortunately," "`cat ${STD_OUT}`"
     assertEquals 1 ${rtrn}
 }
-
-echo "This is a test" > $STD_OUT
-testDeterminePython27
