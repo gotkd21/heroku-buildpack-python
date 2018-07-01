5e075a69d9ff483c216cd33ea3b658f3f198aea4
dmfbrew <gotkd21@gmail.com>
2018-06-30 21:21:41 -0500

    debugging env

--- test/detect_py_test.sh
+++ test/detect_py_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export
-source /apps/test/utils.sh
+source ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
 testDeterminePython27() {
     touch ${BUILD_DIR}/test-runtime.txt
@@ -14,3 +14,4 @@ testDeterminePython27() {
 }
 
 echo "This is a test" > $STD_OUT
+testDeterminePython27
