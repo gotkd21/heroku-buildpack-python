ee610ef3c8125129c3c8ac5ad7e456f8ea8fde34
dmfbrew <gotkd21@gmail.com>
2018-06-30 22:08:40 -0500

    fixed assertion

--- test/detect_py_test.sh
+++ test/detect_py_test.sh
@@ -6,6 +6,12 @@ testDeterminePython27() {
     touch ${BUILD_DIR}/test-runtime.txt
     echo "python-2.7.14" >| ${BUILD_DIR}/test-runtime.txt
 
+    if [ -f ${BUILD_DIR}/test-runtime.txt] then
+        cat ${BUILD_DIR}/test-runtime.txt > $STD_OUT
+        echo "${PYTHON_VERSION}"
+    fi
+
+
     compile
 
     assertContains "Unfortunately" "`cat ${STD_OUT}`"
