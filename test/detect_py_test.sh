abd032f60b1f531fa601ed5b88dd6e6002ddfe13
dmfbrew <gotkd21@gmail.com>
2018-06-30 22:30:28 -0500

    Fix if

--- test/detect_py_test.sh
+++ test/detect_py_test.sh
@@ -3,10 +3,10 @@
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
 testDeterminePython27() {
-    $(touch ${BUILD_DIR}/test-runtime.txt)
+    $(touch "${BUILD_DIR}/test-runtime.txt")
     echo "python-2.7.14" >| ${BUILD_DIR}/test-runtime.txt
 
-    if [ -f "${BUILD_DIR}/test-runtime.txt"]; then
+    if [[ -f "${BUILD_DIR}/test-runtime.txt"]]; then
         $(cat "${BUILD_DIR}/test-runtime.txt") > {$STD_OUT}
         echo "${PYTHON_VERSION}"
     fi
